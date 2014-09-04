e87148ed0d2faf2e37dea3a6e29a1707578fe203
Jeremy Bchiri <jerem.b95@gmail.com>
2014-09-04 13:31:45 +0200

    fin

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-1
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Rattrapage Dev</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<h1>Rattrapage Dev </h1>	
+<?php
+	
+	include 'model/model.php';
+	include 'controller/controller.php';
+	include 'view/vue.php';
+
+?>
+
+</body>
+</html>
